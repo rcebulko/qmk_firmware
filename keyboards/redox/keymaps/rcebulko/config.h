f54a0a4db351dbb17a87b4b4f494adb5be2aecd6
Ryan Cebulko <ryan@cebulko.com>
2022-03-23 12:16:07 -0400

    Fix open parentheses

--- keyboards/redox/keymaps/rcebulko/config.h
+++ keyboards/redox/keymaps/rcebulko/config.h
@@ -34,4 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 
 #define IGNORE_MOD_TAP_INTERRUPT
+// #define PERMISSIVE_HOLD
+
+// #define TAPPING_TOGGLE 2
 
